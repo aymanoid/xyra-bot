332b74a15538cf0630e22ccd1d24b3b3e315f311
Ayman <56372390+Xyrasus@users.noreply.github.com>
2020-03-30 18:54:30 +0100

    feat(cmd): add banner command

---
+++ commands/core/banner.js
@@ -0,0 +1,44 @@
+import { Command } from 'discord-akairo';
+import { MessageEmbed } from 'discord.js';
+
+class BannerCommand extends Command {
+  constructor() {
+    super('banner', {
+      aliases: ['banner'],
+      description: {
+        content: 'Displays the server banner image.',
+      },
+      category: 'Core',
+      clientPermissions: ['EMBED_LINKS'],
+    });
+  }
+
+  async exec(msg) {
+    const currGuild = msg.guild;
+    const botColor = currGuild.me.displayColor;
+    const bannerURL = currGuild.bannerURL({
+      format: 'png',
+      dynamic: true,
+      size: 2048,
+    });
+    const iconURL = currGuild.iconURL({
+      format: 'png',
+      dynamic: true,
+      size: 2048,
+    });
+
+    if (!bannerURL)
+      return msg.channel.send(`This server has no banner image set.`);
+
+    const bannerEmbed = new MessageEmbed()
+      .setColor(botColor || 16777215)
+      .setTitle('Banner URL')
+      .setURL(bannerURL)
+      .setAuthor(currGuild.name, iconURL)
+      .setImage(bannerURL);
+
+    return msg.channel.send(bannerEmbed);
+  }
+}
+
+export default BannerCommand;
