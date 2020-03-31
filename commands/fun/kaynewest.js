264e0c18c28bc8a4d3b0a31985030b1342d0e0d9
Ayman <56372390+Xyrasus@users.noreply.github.com>
2020-03-31 17:56:28 +0100

    feat(cmd): add kaynewest command

---
+++ commands/fun/kaynewest.js
@@ -0,0 +1,23 @@
+import { Command } from 'discord-akairo';
+import axios from 'axios';
+
+class KayneWestCommand extends Command {
+  constructor() {
+    super('kaynewest', {
+      aliases: ['kaynewest', 'kayne'],
+      description: {
+        content: 'Gives a random Kayne West quote.',
+      },
+      category: 'Core',
+    });
+  }
+
+  async exec(msg) {
+    const response = await axios.get('https://api.kanye.rest/?format=text');
+    const result = response.data;
+
+    return msg.channel.send(result);
+  }
+}
+
+export default KayneWestCommand;
