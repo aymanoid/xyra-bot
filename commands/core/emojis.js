13cc0375020776580a05be4259dcaec12afdf460
Ayman <56372390+Xyrasus@users.noreply.github.com>
2020-03-31 12:08:23 +0100

    feat(cmd): add emojis command

---
+++ commands/core/emojis.js
@@ -0,0 +1,62 @@
+import { Command } from 'discord-akairo';
+import { MessageEmbed } from 'discord.js';
+
+class EmojisCommand extends Command {
+  constructor() {
+    super('emojis', {
+      aliases: ['emojis', 'emotes'],
+      description: {
+        content: 'Gets a list of the emotes in a server.',
+      },
+      category: 'Core',
+      clientPermissions: ['EMBED_LINKS'],
+    });
+  }
+
+  async exec(msg) {
+    const currGuild = msg.guild;
+    const currEmojis = currGuild.emojis.cache.array();
+    const botColor = currGuild.me.displayColor;
+
+    const iconURL = currGuild.iconURL({
+      format: 'png',
+      dynamic: true,
+      size: 2048,
+    });
+
+    const chks = [];
+    chks.push('');
+    let n = 0;
+    for (let i = 0; i < currEmojis.length; i += 1) {
+      chks[n] += `${currEmojis[i].toString()} `;
+      if (chks[n].length > 2048) {
+        chks[n] = chks[n].replace(`${currEmojis[i].toString()} `, '');
+        chks.push('');
+        n += 1;
+        i -= 1;
+      }
+    }
+    const emotesEmbed = new MessageEmbed()
+      .setColor(botColor || 16777215)
+      .setAuthor(currGuild.name, iconURL)
+      .setTitle(
+        `${currEmojis.filter((e) => !e.animated).length} Static, ${
+          currEmojis.filter((e) => e.animated).length
+        } Animated`
+      )
+      .setDescription(chks[0] || 'None');
+
+    msg.channel.send(emotesEmbed);
+
+    if (chks.length > 1) {
+      for (let i = 1; i < chks.length; i += 1) {
+        const embed = new MessageEmbed()
+          .setColor(botColor || 16777215)
+          .setDescription(chks[i]);
+        msg.channel.send(embed);
+      }
+    }
+  }
+}
+
+export default EmojisCommand;
