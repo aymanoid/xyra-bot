c2bf76986425bf374fed4742712297f9001e50c6
Ayman <56372390+Xyrasus@users.noreply.github.com>
2020-03-31 13:35:32 +0100

    feat(cmd): add serverinfo command

---
+++ commands/core/serverinfo.js
@@ -0,0 +1,117 @@
+import { Command } from 'discord-akairo';
+import { MessageEmbed } from 'discord.js';
+import moment from 'moment';
+
+class ServerInfoCommand extends Command {
+  constructor() {
+    super('serverinfo', {
+      aliases: ['serverinfo', 'sinfo'],
+      description: {
+        content: 'Gets information about the server.',
+      },
+      category: 'Core',
+      clientPermissions: ['EMBED_LINKS'],
+    });
+  }
+
+  async exec(msg) {
+    const currGuild = msg.guild;
+    const botColor = currGuild.me.displayColor;
+
+    const iconURL = currGuild.iconURL({
+      format: 'png',
+      dynamic: true,
+      size: 2048,
+    });
+
+    const serverAge = moment
+      .duration(moment().diff(currGuild.createdAt))
+      .format(
+        'y [years], M [months], d [days], h [hours], m [minutes], s [seconds]',
+        { largest: 2 }
+      );
+
+    const serverRegion =
+      {
+        'eu-central': 'Central Europe',
+        india: 'India',
+        london: 'London',
+        japan: 'Japan',
+        amsterdam: 'Amsterdam',
+        brazil: 'Brazil',
+        'us-west': 'US West',
+        hongkong: 'Hong Kong',
+        southafrica: 'South Africa',
+        sydney: 'Sydney',
+        europe: 'Europe',
+        singapore: 'Singapore',
+        'us-central': 'US Central',
+        'eu-west': 'Western Europe',
+        dubai: 'Dubai',
+        'us-south': 'US South',
+        'us-east': 'US East',
+        frankfurt: 'Frankfurt',
+        russia: 'Russia',
+      }[currGuild.region] || currGuild.region;
+
+    const serverInfoEmbed = new MessageEmbed()
+      .setColor(botColor || 16777215)
+      .setAuthor(currGuild.name, iconURL)
+      .addField('Owner', currGuild.owner.user.tag, true)
+      .addField('Region', serverRegion, true)
+      .addField('Members', currGuild.memberCount, true)
+      .addField(
+        'Humans',
+        currGuild.members.cache.filter((m) => !m.user.bot).size,
+        true
+      )
+      .addField(
+        'Bots',
+        currGuild.members.cache.filter((m) => m.user.bot).size,
+        true
+      )
+      .addField(
+        'Online',
+        currGuild.members.cache.filter((m) => m.presence.status !== 'offline')
+          .size,
+        true
+      )
+      .addField(
+        `Level ${currGuild.premiumTier}`,
+        `${
+          currGuild.premiumSubscriptionCount
+            ? currGuild.premiumSubscriptionCount
+            : 0
+        } Boosts`,
+        true
+      )
+      .addField('Roles', currGuild.roles.cache.size, true)
+      .addField('Emotes', currGuild.emojis.cache.size, true)
+      .addField(
+        'Channel Categories',
+        currGuild.channels.cache.filter((c) => c.type === 'category').size,
+        true
+      )
+      .addField(
+        'Text Channels',
+        currGuild.channels.cache.filter((c) => c.type === 'text').size,
+        true
+      )
+      .addField(
+        'Voice Channels',
+        currGuild.channels.cache.filter((c) => c.type === 'voice').size,
+        true
+      )
+      .setFooter(
+        `ID: ${currGuild.id} | Created On: ${moment(currGuild.createdAt).format(
+          'llll z'
+        )} (${serverAge} ago)`
+      );
+
+    if (iconURL) serverInfoEmbed.setTitle('Icon URL').setURL(iconURL);
+
+    return msg.channel.send(serverInfoEmbed);
+  }
+}
+
+export default ServerInfoCommand;
