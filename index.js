d735ceb7193366fe64078807809c46649919fd2d
Ayman <56372390+Xyrasus@users.noreply.github.com>
2020-04-01 06:36:51 +0100

    feat: use Logger in index

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 import 'dotenv/config';
 import moment from 'moment-timezone';
 import XyraClient from './struct/XyraClient';
+import Logger from './util/Logger';
 
 moment.tz.setDefault('UTC');
 
-const client = new XyraClient();
-const token = process.env.TOKEN;
-client.start(token);
+const client = new XyraClient(process.env);
 
-process.on('unhandledRejection', (err) => console.error(err)); // eslint-disable-line no-console
+client
+  .on('disconnect', () => Logger.warn('Connection lost...'))
+  .on('reconnect', () => Logger.info('Attempting to reconnect...'))
+  .on('error', (err) => Logger.error(err))
+  .on('warn', (info) => Logger.warn(info));
+
+client.start();
+
+process.on('unhandledRejection', (err) => {
+  Logger.error('An unhandled promise rejection occured');
+  Logger.stacktrace(err);
+});
