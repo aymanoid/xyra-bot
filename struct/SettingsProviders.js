65d3798934ad299d5b60aaac56db25696030e75c
Ayman <56372390+Xyrasus@users.noreply.github.com>
2020-04-02 02:51:21 +0100

    feat(db): add SettingsProviders struct

---
+++ struct/SettingsProviders.js
@@ -0,0 +1,42 @@
+import { Guild } from 'discord.js';
+import { SequelizeProvider } from 'discord-akairo';
+
+class SettingsProvider extends SequelizeProvider {
+  constructor(table) {
+    super(table, {
+      idColumn: 'guildID',
+      dataColumn: 'settings',
+    });
+  }
+
+  get(guild, key, defaultValue) {
+    const id = this.constructor.getGuildID(guild);
+    return super.get(id, key, defaultValue);
+  }
+
+  set(guild, key, value) {
+    const id = this.constructor.getGuildID(guild);
+    return super.set(id, key, value);
+  }
+
+  delete(guild, key) {
+    const id = this.constructor.getGuildID(guild);
+    return super.delete(id, key);
+  }
+
+  clear(guild) {
+    const id = this.constructor.getGuildID(guild);
+    return super.clear(id);
+  }
+
+  static getGuildID(guild) {
+    if (guild instanceof Guild) return guild.id;
+    if (guild === 'global' || guild === null) return 'global';
+    if (typeof guild === 'string' && /^\d+$/.test(guild)) return guild;
+    throw new TypeError(
+      'Invalid guild specified. Must be a Guild instance, guild ID, "global", or null.'
+    );
+  }
+}
+
+export default SettingsProvider;
