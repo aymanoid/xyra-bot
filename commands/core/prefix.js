1efc96c6a36264c8d1dcb7d22f8cef1f5425da93
Ayman <56372390+Xyrasus@users.noreply.github.com>
2020-04-02 09:01:10 +0100

    feat(cmd): add prefix command

---
+++ commands/core/prefix.js
@@ -0,0 +1,46 @@
+import { Command, Argument } from 'discord-akairo';
+
+class PrefixCommand extends Command {
+  constructor() {
+    super('prefix', {
+      aliases: ['prefix'],
+      description: {
+        content: 'Shows the current prefix, or sets a new one.',
+        usage: '[emoji]',
+      },
+      category: 'Core',
+      clientPermissions: ['EMBED_LINKS'],
+      args: [
+        {
+          id: 'prefix',
+          match: 'content',
+          type: Argument.validate(
+            'string',
+            (m, p) => !/\s/.test(p) && p.length <= 10
+          ),
+          prompt: {
+            start: 'What would you like to set the prefix to?',
+            retry:
+              'Please provide a prefix without spaces and less than 10 characters.',
+            optional: true,
+          },
+        },
+      ],
+    });
+  }
+
+  async exec(msg, { prefix }) {
+    if (!prefix) {
+      const currentPrefix = this.client.settings.get(msg.guild, 'prefix');
+      return msg.channel.send(
+        `The prefix of this server is \`${currentPrefix}\``
+      );
+    }
+    await this.client.settings.set(msg.guild, 'prefix', prefix);
+    if (prefix === '$$')
+      return msg.channel.send('Prefix has been reset to `$$`');
+    return msg.channel.send(`Prefix has been set to \`${prefix}\``);
+  }
+}
+
+export default PrefixCommand;
