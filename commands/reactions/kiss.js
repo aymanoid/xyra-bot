3f9be5c1e7ba3c118f191bf96ccd6e98126ad75d
Ayman <56372390+Xyrasus@users.noreply.github.com>
2020-04-02 18:35:27 +0100

    feat(cmd): add kiss command

---
+++ commands/reactions/kiss.js
@@ -0,0 +1,48 @@
+import { Command, Argument } from 'discord-akairo';
+import { MessageEmbed } from 'discord.js';
+import NekosLife from 'nekos.life';
+
+class KissCommand extends Command {
+  constructor() {
+    super('kiss', {
+      aliases: ['kiss'],
+      description: {
+        content: 'Kiss someone.',
+        usage: '<member>',
+      },
+      category: 'Reactions',
+      channel: 'guild',
+      clientPermissions: ['EMBED_LINKS'],
+      args: [
+        {
+          id: 'meOrMember',
+          type: Argument.union(['me'], 'member'),
+          otherwise: 'You need to mention someone to kiss them.',
+        },
+      ],
+    });
+  }
+
+  async exec(msg, args) {
+    const botColor = msg.guild.me.displayColor;
+
+    const trgMember = args.meOrMember === 'me' ? msg.member : args.meOrMember;
+
+    const description =
+      msg.member.id === trgMember.id
+        ? `**${msg.member.displayName}** kisses themselves`
+        : `**${msg.member.displayName}** kisses **${trgMember.displayName}**`;
+
+    const nekosClient = new NekosLife();
+    const imageURL = (await nekosClient.sfw.kiss()).url;
+
+    const actionEmbed = new MessageEmbed()
+      .setColor(botColor || 16777215)
+      .setDescription(description)
+      .setImage(imageURL);
+
+    return msg.channel.send(actionEmbed);
+  }
+}
+
+export default KissCommand;
