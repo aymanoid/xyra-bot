370e10c531cec0537ea06197c385f45d76472fca
Ayman <56372390+Xyrasus@users.noreply.github.com>
2020-04-07 18:19:25 +0100

    chore(constants): add EMOJIS constant

---
+++ util/Constants.js
@@ -0,0 +1,7 @@
+// eslint-disable-next-line import/prefer-default-export
+export const EMOJIS = {
+  SUCCESS: ':white_check_mark:',
+  ERROR: ':x:',
+  INFO: ':information_source:',
+  NOTIFY: ':bell:',
+};
