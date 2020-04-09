77a2899214bdb2a60ffd60ccd77c1c1f8e6d6ab2
Ayman <56372390+Xyrasus@users.noreply.github.com>
2020-04-09 01:36:20 +0100

    feat(cmd): add customscript command

---
+++ commands/private/customscript.js
@@ -0,0 +1,47 @@
+import { Command } from 'discord-akairo';
+import axios from 'axios';
+import NekosLife from 'nekos.life';
+
+class CustomScriptCommand extends Command {
+  constructor() {
+    super('customscript', {
+      aliases: ['customscript', 'c'],
+      description: {
+        content: 'Executes a premade script.',
+      },
+      category: 'private',
+      ownerOnly: true,
+      quoted: false,
+      args: [
+        {
+          id: 'script',
+        },
+        {
+          id: 'text1',
+        },
+        {
+          id: 'text2',
+        },
+      ],
+    });
+  }
+
+  async exec(msg, args) {
+    switch (args.script.toLowerCase()) {
+      case 'nekoslife' || 'nl': {
+        const imageURL = (await new NekosLife()[args.text1][args.text2]()).url;
+        return msg.channel.send(imageURL);
+      }
+      case 'nekobot' || 'nb': {
+        const imageURL = (
+          await axios.get(`https://nekobot.xyz/api/image?type=${args.text1}`)
+        ).data.message;
+        return msg.channel.send(imageURL);
+      }
+      default:
+        return null;
+    }
+  }
+}
+
+export default CustomScriptCommand;
