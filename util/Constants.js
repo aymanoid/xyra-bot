e06a9c953de41e38a5f305f75b1ccf13778fc6b3
Ayman <56372390+Xyrasus@users.noreply.github.com>
2020-04-17 09:38:50 +0100

    feat(util): add custom emojis in constants

--- util/Constants.js
+++ util/Constants.js
@@ -1,8 +1,8 @@
 // eslint-disable-next-line import/prefer-default-export
 export const EMOJIS = {
-  SUCCESS: ':white_check_mark:',
-  ERROR: ':x:',
-  INFO: ':information_source:',
-  LOADING: ':sparkle:',
-  NOTIFY: ':bell:',
+  SUCCESS: '<:success:700578053281153094>',
+  ERROR: '<:error:700578066581159997>',
+  INFO: '<:info:700578081185988689>',
+  LOADING: '<:loading:700578091705040967>',
+  INSTAGRAM_VERIFIED: '<:InstagramVerified:700519496732901454>',
 };
