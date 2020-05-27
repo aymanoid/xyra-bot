8faebe976792182fef404c5dbae8b03e315142a0
Ayman <56372390+Xyrasus@users.noreply.github.com>
2020-05-27 09:49:13 +0000

    feat(cmd): add tiktok command

---
+++ commands/search/tiktok.js
@@ -0,0 +1,73 @@
+import { Command } from 'discord-akairo';
+import { MessageEmbed } from 'discord.js';
+import * as TikTokScraper from 'tiktok-scraper';
+import { EMOJIS } from '../../util/Constants';
+
+class TikTokCommand extends Command {
+  constructor() {
+    super('tiktok', {
+      aliases: ['tiktok'],
+      description: {
+        content: 'Gets info about a TikTok account.',
+        usage: '[username]',
+      },
+      category: 'search',
+      channel: 'guild',
+      clientPermissions: ['EMBED_LINKS'],
+      cooldown: 5000,
+      args: [
+        {
+          id: 'username',
+        },
+      ],
+    });
+  }
+
+  async exec(msg, args) {
+    if (!args.username)
+      return msg.channel.send(`${EMOJIS.ERROR} No username was provided.`);
+
+    let userProfileInfo;
+    try {
+      userProfileInfo = await TikTokScraper.getUserProfileInfo(args.username);
+    } catch (error) {
+      return msg.channel.send(
+        `${EMOJIS.ERROR} No account found with that username.`
+      );
+    }
+
+    const title = userProfileInfo.nickName
+      ? `${userProfileInfo.nickName} (@${userProfileInfo.uniqueId})`
+      : `@${userProfileInfo.uniqueId}`;
+    const verified = userProfileInfo.verified
+      ? ` ${EMOJIS.TIKTOK_VERIFIED}`
+      : '';
+    const profileURL = `https://www.tiktok.com/@${userProfileInfo.uniqueId}`;
+    const { signature } = userProfileInfo;
+    const profilePicURL = userProfileInfo.coversMedium[0];
+    const videos = userProfileInfo.video.toLocaleString();
+    const favorites = userProfileInfo.digg.toLocaleString();
+    const privacy = userProfileInfo.isSecret ? 'Private' : 'Public';
+    const following = userProfileInfo.following.toLocaleString();
+    const followers = userProfileInfo.fans.toLocaleString();
+    const likes = Number(userProfileInfo.heart).toLocaleString();
+    const id = userProfileInfo.userId;
+
+    const tiktokEmbed = new MessageEmbed()
+      .setTitle(`${title}${verified}`)
+      .setURL(profileURL)
+      .setDescription(signature)
+      .setThumbnail(profilePicURL)
+      .addField('Videos', videos, true)
+      .addField('Favorites', favorites, true)
+      .addField('Privacy', privacy, true)
+      .addField('Following', following, true)
+      .addField('Followers', followers, true)
+      .addField('Likes', likes, true)
+      .setFooter(`TikTok • ID: ${id}`, 'https://logo.clearbit.com/tiktok.com');
+
+    return msg.channel.send(tiktokEmbed);
+  }
+}
+
+export default TikTokCommand;
