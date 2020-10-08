0d247e57942e8c7a895e6382994badb532bb2901
Ayman <56372390+Xyrasus@users.noreply.github.com>
2020-10-08 14:28:23 +0100

    feat(cmd): add ghostping command

---
+++ commands/general/ghostping.js
@@ -0,0 +1,49 @@
+import { Command, Argument } from 'discord-akairo';
+import { EMOJIS } from '../../util/Constants';
+
+class GhostPingCommand extends Command {
+  constructor() {
+    super('ghostping', {
+      aliases: ['ghostping'],
+      description: {
+        content: 'Sets the channel for ghost ping, or disables it.',
+        usage: '[channel] | disable',
+      },
+      category: 'general',
+      channel: 'guild',
+      clientPermissions: ['MANAGE_GUILD'],
+      userPermissions: ['MANAGE_GUILD'],
+      args: [
+        {
+          id: 'channel',
+          match: 'content',
+          type: Argument.union(['disable'], 'channel'),
+        },
+      ],
+    });
+  }
+
+  async exec(msg, args) {
+    if (args.channel === 'disable') {
+      await this.client.settings.set(msg.guild, 'ghostPingChannel', null);
+      return msg.channel.send(
+        `${EMOJIS.SUCCESS}  The ghost ping has been disabled in this server.`
+      );
+    }
+    if (!args.channel)
+      return msg.channel.send(`${EMOJIS.ERROR} No valid channel was provided.`);
+
+    await this.client.settings.set(
+      msg.guild,
+      'ghostPingChannel',
+      args.channel.id
+    );
+    return msg.channel.send(
+      `${
+        EMOJIS.SUCCESS
+      } ${args.channel.toString()} has been set as the ghost ping channel.`
+    );
+  }
+}
+
+export default GhostPingCommand;
